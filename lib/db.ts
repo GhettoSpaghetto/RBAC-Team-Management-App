7992aa9329a1f0b054e90f327eb4600662f75213
Samarth Kaushal <samarth.chikki.142@gmail.com>
2026-02-26 21:43:26 +0530

    fix(db): refactor PrismaClient initialization and update database connection check feat(migration): add initial migration for users and teams tables with roles chore(package): update build script to include Prisma commands and remove unused dependencies

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// Database helper function
-export async function checkDatabaseConnection(): Promise<boolean>{
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export async function checkDatabaseConnection(): Promise<boolean> {
   try {
-    await prisma.$queryRaw`Select 1`;
-    return true
-
-
+    await prisma.$queryRaw`SELECT 1`;
+    return true;
   } catch (error) {
-    console.error(`Database connection failed : ${error}`  );
+    console.error(`Database connection failed: ${error}`);
     return false;
   }
 }
